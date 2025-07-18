7b4b5c07e7234c160d59ef357a686192ab0b5652
githbuser13578 <cleghorngiles@gmail.com>
2025-07-19 01:50:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ load_dotenv()
 app = Flask(__name__)
 
 EBAY_APP_ID = os.getenv(GilesCle-A-SBX-f0bb589a9-f66214b5)
-EBAY_DEV_ID = os.getenv(9c190ab3-af18-458d-9d8c-49a4c10ddea4)
+EBAY_DEV_ID = os.getenv("9c190ab3-af18-458d-9d8c-49a4c10dea4")
+
 EBAY_CERT_ID = os.getenv(SBX-0bb589a9da6c-631f-4849-94b0-ee43)
 
 EBAY_API_URL = "https://svcs.ebay.com/services/search/FindingService/v1"
