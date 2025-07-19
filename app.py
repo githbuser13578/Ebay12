01346d22080a18117beeaaf4c6fa811370c6b65e
githbuser13578 <cleghorngiles@gmail.com>
2025-07-19 10:17:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
+from flask import Flask, jsonify, request
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Ensure the app instance is created
+app = Flask(__name__)
+
+EBAY_APP_ID = os.getenv("GilesCle-A-SBX-f0bb589a9a6c-631f-4849-94b0-f66214b5")
+EBAY_DEV_ID = os.getenv("9c190ab3-af18-458d-9d8c-49a4c10dea4")
+EBAY_CERT_ID = os.getenv("SBX-0bb589a9da6c-631f-4849-94b0-ee43")
+
+EBAY_API_URL = "https://svcs.ebay.com/services/search/FindingService/v1"
+
+@app.route('/')
+def index():
+    return "Welcome to the eBay Relister!"
+
 @app.route('/fetch_listings', methods=['GET'])
 def fetch_listings():
     query = request.args.get('query', '')
@@ -25,4 +45,5 @@ def fetch_listings():
 
     # Return mock data
     return jsonify(mock_data)
+
 
